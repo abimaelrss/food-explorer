a43c1eeb13ebb0e972a1362bf9697ba2b29a9781
Abimael <abimaelrss@gmail.com>
2023-10-11 10:54:50 -0300

    alteração no seed de usuário pra gerar senha criptografada

--- food-explorer-backend/src/database/knex/seeds/initUsers.js
+++ food-explorer-backend/src/database/knex/seeds/initUsers.js
@@ -2,16 +2,22 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
+
+const { hash } = require("bcryptjs");
+
 exports.seed = async function (knex) {
+  const password = "123";
+  const hashedPassword = await hash(password, 8);
+
   // Deletes ALL existing entries
   // await knex("table_name").del();
   await knex("users").truncate();
   await knex("users").insert([
     {
       id: 1,
-      name: "admin",
+      name: "Administrator",
       email: "admin@email.com",
-      password: "123",
+      password: `${hashedPassword}`,
       role: "admin",
     },
   ]);
